57ea86a198cb2890b355d0a41953084a9113352a
Niranjan A S <niranjan0881@gmail.com>
2024-06-20 13:59:25 +0530

    chore: sample app

---
+++ app-1/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { connectToDatabase } from './helper.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hi there!</h1>');
+});
+
+const db = await connectToDatabase();
+console.log(db);
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
